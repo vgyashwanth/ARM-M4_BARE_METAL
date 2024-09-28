7162258deb5f3cf4df688b3d3ce22b14ca7b788e
VIDAPANAKAL GANGAPPAGARI YASHWANTH KUMAR <83589480+vgyashwanth@users.noreply.github.com>
2024-09-29 02:55:03 +0530

    Update startup.c

--- startup.c
+++ startup.c
@@ -11,6 +11,12 @@ extern uint32_t _etext;
 extern uint32_t _sbss;
 extern uint32_t _ebss;
 
+/* copy the data from .data(FLASH) to .data(SRAM) */
+/* why global ? to avoid the disturbance of the stack pointer value  */
+uint32_t size = &_edata - &_sdata;
+uint32_t * ptrSrc = &_etext;
+uint32_t * ptrDes = &_sdata;
+int i;
 /*__attribute__ ((section(".isr_vec"))) will create a section .isr_vec(user dependent) stores their instead of storing in .data*/
 uint32_t InterruptVectorTable[101] __attribute__ ((section(".isr_vec"))) = {
 
@@ -20,25 +26,23 @@ uint32_t InterruptVectorTable[101] __attribute__ ((section(".isr_vec"))) = {
       /* rest need to add refering to microcontroller Interrupt vector table */
 
 };
-
+/*calling this function may disturb msp value*/
 void Reset_Handler(void){
 
-    /* copy the data from .data(FLASH) to .data(SRAM) */
-    uint32_t size = &_edata - &_sdata;
-    uint32_t * ptrSrc = &_etext;
-    uint32_t * ptrDes = &_sdata;
-    for(int i=0;i<size;i++){
+
+    for(i=0;i<size;i++){
         ptrDes[i] = ptrSrc[i];
     }
     size = &_ebss - &_sbss;
     ptrDes = &_sbss;
 
     /* Initialise the .bss section */
-      for(int i=0;i<size;i++){
+      for(i=0;i<size;i++){
         ptrDes[i] = 0 ;
     }
 
     /*Calling main function*/
+    /*calling this function may disturb msp value*/ 
     main();
 
 }
